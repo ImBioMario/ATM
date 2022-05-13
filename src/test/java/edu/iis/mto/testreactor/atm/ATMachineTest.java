1e64852782286bcf8a17fd1e94499d2acb1098e1
ImBioMario <m.sukiennik95@gmail.com>
2022-05-13 14:46:28 +0200

    Added test for successful withdrawal and checking if the deposit state is as should be. Added test for failed authorization.

--- src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
+++ src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
@@ -2,9 +2,14 @@ package edu.iis.mto.testreactor.atm;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 
+import edu.iis.mto.testreactor.atm.bank.AuthorizationException;
 import edu.iis.mto.testreactor.atm.bank.Bank;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -25,7 +30,7 @@ class ATMachineTest {
     ATMachine atm;
     List<BanknotesPack> banknotes;
     @BeforeEach
-    void setUp() throws Exception {
+    void setUp() {
         atm = new ATMachine(bank, Currency.getInstance("PLN"));
         banknotes = new ArrayList<>();
         pin = PinCode.createPIN(1,2,3,4);
@@ -43,6 +48,29 @@ class ATMachineTest {
         correctWithdraw.add(BanknotesPack.create(10, Banknote.PL_10));
 
         assertEquals(atm.withdraw(pin, card, new Money(100)), Withdrawal.create(correctWithdraw));
+    }
+
+    @Test
+    void succesfulWithdrawalAndCorrectAmountOfDepositLeft() throws ATMOperationException {
+        BanknotesPack hundreds = BanknotesPack.create(100, Banknote.PL_100);
+        banknotes.add(hundreds);
+        MoneyDeposit deposit = MoneyDeposit.create(atm.getCurrentDeposit().getCurrency(), banknotes);
+        atm.setDeposit(deposit);
+
+        List<BanknotesPack> correctWithdraw = new ArrayList<>();
+        correctWithdraw.add(BanknotesPack.create(10, Banknote.PL_100));
+
+        assertEquals(atm.withdraw(pin, card, new Money(1000)), Withdrawal.create(correctWithdraw));
+        int banknotesLeft = atm.getCurrentDeposit().getBanknotes().get(0).getCount();
+        assertEquals(banknotesLeft, 100-10);
+    }
+
+    @Test
+    void failedWithdrawalWithFailedAuthorization() throws AuthorizationException {
+
+        doThrow(AuthorizationException.class).when(bank).autorize(any(), any());
+        ATMOperationException errCode = assertThrows(ATMOperationException.class, () -> atm.withdraw(pin, card, new Money(100)));
+        assertEquals(errCode.getErrorCode(), ErrorCode.AUTHORIZATION);
     }
 
 }
